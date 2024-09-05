fc2dfb88cc7e55cb2eb5f5d55cd54bf7a1665ad7
Carlos Santana <carlosgabriel46@gmail.com>
2024-09-05 15:04:22 -0600

    Se vincula repositorio local con GitHub y se terminan de modificar

---
+++ js/index.js
@@ -0,0 +1,34 @@
+let divData = document.getElementById("divData")
+
+function getData(){
+    const promesa = fetch ("https://freetestapi.com/api/v1/products",{method: "GET"});
+    promesa.then( (response) => {
+            response.json().then(
+                (data)=>{
+                    console.log(data);
+                    createCards(data);
+        }).catch((error)=> console.log("Prolema con el json", error));
+    }).catch((err)=> console.log("Existió un problema con la solicitud", err));
+}//getData
+
+function createCards(products){
+    console.log(products.length);
+    console.log(products[0].name);
+
+    products.forEach(product => {
+        divData.insertAdjacentHTML("beforeend",
+            `<div class="card" style="width: 18rem;">
+      <img src="${product.image}" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">${product.name}</h5>
+        <p class="card-text">${product.description}</p>
+        <p class="card-text">Precio <strong>$${product.price}</strong></p>
+        <a href="#" class="btn btn-primary">Comprar</a>
+      </div>
+    </div>`)
+    })
+    
+    //crear una Card por cada producto con sus datos esenciales
+    // de preferencia foreach: name, description, image, price
+}//createCards
+getData();
