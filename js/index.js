5dc7f02247894ef298add75184f13d03cbea5264
Carlos Santana <carlosgabriel46@gmail.com>
2024-09-05 15:34:30 -0600

    Se agrega caracteristica para caracteres maximos en descripcion de las cards

--- js/index.js
+++ js/index.js
@@ -21,7 +21,7 @@ function createCards(products){
       <img src="${product.image}" class="card-img-top" alt="...">
       <div class="card-body">
         <h5 class="card-title">${product.name}</h5>
-        <p class="card-text">${product.description}</p>
+        <p class="card-text">${product.description.slice(0,20)}...</p>
         <p class="card-text">Precio <strong>$${product.price}</strong></p>
         <a href="#" class="btn btn-primary">Comprar</a>
       </div>
